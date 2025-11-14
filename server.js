04f9c0960c004e9b457281237663341d144476b1
octrooier <thatyellowsparrow@gmail.com>
2025-11-14 23:12:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-ECHO is on.
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', (msg) => {
+    console.log('Received:', msg);
+    ws.send(Echo: ${msg});
+  });
+});
+
+const PORT = process.env.PORT || 1000;
+server.listen(PORT, () => {
+  console.log(Server running on port ${PORT});
+})
